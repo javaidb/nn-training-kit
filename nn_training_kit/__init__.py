9624ea8b57264e2dfff83c6bddd14e6e6d2d77fa
Javaid Baksh <javaidbaksh@gmail.com>
2025-04-05 21:55:58 -0400

    Adding version number

---
+++ nn_training_kit/__init__.py
@@ -0,0 +1,8 @@
+"""Neural Network Training Kit.
+
+A toolkit for neural network training with PyTorch Lightning.
+"""
+
+from nn_training_kit.__version__ import __version__
+
+__all__ = ["__version__"] 
